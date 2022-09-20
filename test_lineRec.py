a277052724e4837b7435da67d732aef65c22ee30
PhillipTablet <philliprothenbeck@gmail.com>
2022-09-20 17:55:47 +0200

    set right threshhold

--- test_lineRec.py
+++ test_lineRec.py
@@ -7,18 +7,39 @@ import xtf_png
 
 #xtf_png.xtf2png('2019apr04_ecker_sued_10002.xtf', 'formated.png')
 
-img = cv2.imread('MARK0011_enh.png')
+img = cv2.imread('MARK0019.png')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-"""
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
-"""
+clahe = cv2.createCLAHE(clipLimit=1)
+cl1 = clahe.apply(gray) + 10
+clahe_images = np.concatenate((cl1, gray), axis=1)
 
-low_threshold = 50
-high_threshold = 150
-edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+print(len(cl1))
+
+graycl = cv2.cvtColor(cl1, cv2.COLOR_BGR2GRAY)
+
+kernel_size = 5
+blur_gray = cv2.GaussianBlur(graycl, (kernel_size, kernel_size), 0)
+
+
+low_threshold0 = 0
+high_threshold0 = 75
+edges0 = cv2.Canny(blur_gray, low_threshold0, high_threshold0)
+cv2.imshow("edges0", edges0)
+
+
+low_threshold1 = 50
+high_threshold1 = 150
+edges1 = cv2.Canny(blur_gray, low_threshold1, high_threshold1)
+cv2.imshow("edges1", edges1)
+
+low_threshold2 = 50
+high_threshold2 = 400
+edges2 = cv2.Canny(blur_gray, low_threshold2, high_threshold2)
+cv2.imshow("edges2", edges2)
+
+cv2.waitKey(0)
 
 """
 rho = 1  # distance resolution in pixels of the Hough grid
