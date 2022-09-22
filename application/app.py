2f1dcd91239dd506c5636011a019c4543eeea457
Unknown <andreimoisaa@mail.ru>
2022-09-22 16:29:38 +0200

    gui wip

--- application/app.py
+++ application/app.py
@@ -1,6 +1,8 @@
+from enum import auto
 import os
 from tkinter import *
 from tkinter import filedialog
+from tkinter.ttk import Style
 import xtf_png
 # äimport findingProcessor
 
@@ -58,29 +60,38 @@ def delete_selection():
     build_label_text()
 
 
+#Root
 root = Tk()
 root.title('Geisternetz Finder')
 root.geometry("500x250")
 root.resizable(False, False) 
 root.configure(background='darkturquoise')
+
+# Buttons
+style = Style()
+style.configure('TButton', font =
+               ('calibri', 20, 'bold'),
+                    borderwidth = '4')
+style.map('TButton', foreground = [('active', '!disabled', 'green')],
+                     background = [('active', 'black')])
 find_files = Button(root, text="Browse Files", command=browseFiles)
-find_files.grid(column=1, row=1, sticky='w')
+find_files.grid(column=1, row=1, sticky='w',pady = 10, padx=10)
 
 start_process = Button(root, text="Analyze",
                        command=start_processing, state='disabled')
-start_process.grid(column=3, row=1)
+start_process.grid(column=3, row=1,padx = 10,sticky='w')
 
 delete_button = Button(root, text='Delete Selection',
                        comman=delete_selection, bg="red")
-delete_button.grid(column=2, row=1)
+delete_button.grid(column=2, row=1,pady = 10)
 
 selected_files_label = Label(root,
                              text="",
                              width=0, height=4,
                              fg="blue",
                              anchor='w',
-                             justify=LEFT)
+                             justify=CENTER)
 build_label_text()
-selected_files_label.grid(column=1, row=4, sticky='w')
+selected_files_label.grid(columnspan=3, row=2,padx=10)
 
 mainloop()
