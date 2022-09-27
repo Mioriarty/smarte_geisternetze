a82cbb76a17acc3a8cf59687c136fb1bff9fc345
miamymia <mschumacher@geomar.de>
2022-09-27 13:01:50 +0200

    corrected offset sign, added comments

--- application/calc_lag.py
+++ application/calc_lag.py
@@ -14,11 +14,17 @@ def convert_coords(la,lo,layback,heading,offset):
     EAST_lag = np.float(EAST - layback*math.sin(heading))
     NORTH_lag = np.float(NORTH - layback*math.cos(heading))
 
+    # calculate difference for x,y, components of original point and layback point
+    # and flip vector to make it point in forward/ship movement direction (*(-1))
     DIFF_E = EAST - EAST_lag
-    DIFF_N = NORTH-NORTH_lag
-    DIFF = np.array([DIFF_E, DIFF_N])
+    DIFF_N = NORTH - NORTH_lag
+    DIFF = (np.array([DIFF_E, DIFF_N]))*(-1)
+
+    # rotate DIFF vector in 90° to get offset in x-direction
     rot_mat = np.matrix([[0,1],[-1,0]])
     ROT = np.dot(rot_mat,DIFF)
+
+    # normalise rotated DIFF vector 
     ROT = ROT/np.linalg.norm(ROT)*offset
 
     ROT_E = EAST_lag + ROT[0,0]
@@ -31,5 +37,8 @@ def convert_coords(la,lo,layback,heading,offset):
     LAT_lag = Deg_lag[1]
 
 
-    return LAT_lag, LON_lag
+    return LAT_lag, LON_lag, ROT_E
+
+a = convert_coords(54.23,9.78,10,30,3)
+print(a)
     
