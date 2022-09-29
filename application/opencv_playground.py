98d4a46dec208c4ded45bc757c5fa66671859a3d
Lethey2552 <73588822+Lethey2552@users.noreply.github.com>
2022-09-29 13:13:37 +0200

    changed file structure and wrote comments

--- application/opencv_playground.py
+++ application/opencv_playground.py
@@ -1,4 +1,5 @@
 import glob
+import math
 import cv2
 import numpy as np
 import multiprocessing
@@ -22,6 +23,8 @@ def loopOverImages(dir):
 
     return findings
 
+# read image slice and mask, convert them to grayscale, apply histogram equalization, resize the images
+# apply non local means denoising
 def imgFiltering(url, maskUrl):
     img = cv2.imread(url)
     imgMask = cv2.imread(maskUrl)
@@ -42,11 +45,9 @@ def imgFiltering(url, maskUrl):
 
     findings = getFinding(contours, url, scaleFactor)
 
-    # if len(findings) > 0: 
-    #     display(str(findings[0].pixelCoord) + url[30:], np.concatenate((imgGray, cl1, image, singlePixel), axis=1))
-
     return findings
 
+# limit contour size and return all contours as findings for one image slice
 def getFinding(contours, url, scaleFactor):
     findings = []
 
@@ -64,30 +65,28 @@ def getFinding(contours, url, scaleFactor):
 
     return findings
 
+# display the image and discard window on key press
 def display(windowName, images):
     cv2.imshow(windowName, images)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+# resize an image by a scaling factor
 def resize(image, scaleFactor):
     width = int(image.shape[1] * scaleFactor)
     height = int(image.shape[0] * scaleFactor)
     dim = (width, height)
     return cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
 
+# applying bottom detection mask, edge detection and contour filtering
 def detectEdgesAndDisplay(imgMask, cl1):
+
     # Canny Edge Detection good values for unedited images th1=75 th2=225
     cl1Edges = cv2.Canny(image=cl1, threshold1=75, threshold2=225)
     cl1Edges = cv2.bitwise_and(cl1Edges, cl1Edges, mask=imgMask)
     cl1Edges = cv2.morphologyEx(cl1Edges, cv2.MORPH_CLOSE, kernel=np.ones((2,2), np.uint8))
     cl1Edges = cv2.dilate(cl1Edges, kernel=np.ones((3,3), np.uint8), iterations=1)
-    contours, hierachy = cv2.findContours(cl1Edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # contourImage = np.zeros((cl1.shape[0], cl1.shape[1]), dtype=np.uint8)
-
-    # for i in range(len(contours)):
-    #     if(isContourLine(contours[i])):
-    #         cv2.drawContours(contourImage, contours, i, (255, 255, 255), 2, cv2.LINE_4, hierachy, 0)
+    contours, _ = cv2.findContours(cl1Edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     contours = [c for c in contours if isContourLine(c)]
 
@@ -96,36 +95,21 @@ def detectEdgesAndDisplay(imgMask, cl1):
         contours = contours[:i+1] + [ c for c in contours[i+1:] if distance(cv2.minEnclosingCircle(c)[0], cv2.minEnclosingCircle(contours[i])[0]) > 100]
         i+=1
 
-
     return contours
 
+# calculating the distance between two points
 def distance(mid1, mid2):
-    return int(np.sqrt((mid2[0] - mid1[0])**2 + (mid2[1] - mid1[1])**2))
+    # return int(np.sqrt((mid2[0] - mid1[0])**2 + (mid2[1] - mid1[1])**2))
+    return math.dist(mid2, mid1)
 
+# check if contour satisfies circuference/area * radius condition
 def isContourLine(contour):
     hull = cv2.convexHull(contour)
-    hullArea = cv2.contourArea(hull)
-    hullPerimeter = cv2.arcLength(hull, True)
-
     _, radius = cv2.minEnclosingCircle(contour)
 
-    coefficient = hullPerimeter/hullArea * radius
+    coefficient = cv2.arcLength(hull, True)/cv2.contourArea(hull) * radius
 
     return coefficient > 4.2
 
-def blurAndDisplay(imgGray, cl1):
-    imgGrayBlur = cv2.GaussianBlur(imgGray, (7,7), 0)
-    imgCl1Blur = cv2.GaussianBlur(cl1, (7,7), 0)
-
-    # Canny Edge Detection
-    grayBlurEdges = cv2.Canny(image=imgGrayBlur, threshold1=30, threshold2=200)
-    cl1BlurEdges = cv2.Canny(image=imgCl1Blur, threshold1=30, threshold2=200) # Canny Edge Detection
-
-    # Display Canny Edge Detection Image
-    return np.concatenate((imgGray, cl1, grayBlurEdges, cl1BlurEdges),axis=1)
-
-
 if __name__ == '__main__':
     print(len(loopOverImages("./res/cutted_images/unedited/")))
-    # imgFiltering("./res/cutted_images/unedited/2019apr04_ecker_sued_10002_16650_bot.png", 
-    #              "./res/cutted_images/unedited/2019apr04_ecker_sued_10002_16650_bot_mask.png")
